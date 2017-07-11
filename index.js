763e8e761ae60193b9a067087e848d51c9613497
iSkore <iSkore@users.noreply.github.com>
2017-07-11 18:35:42 -0400

    updated to ES6 typed

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-var os = require('os');
-function getMyIP(version,internal){
-  version = version || 'IPv4';
-  internal = internal || false;
-  var interfaces = os.networkInterfaces();
-  for(var key in interfaces){
-    var addresses = interfaces[key];
-    for(var i = 0; i < addresses.length; i++){
-      var address = addresses[i];
-      if(address.internal === internal && address.family === version){
-        return address.address;
-      };
-    };
-  };
-  return 'localhost'
-};
+'use strict';
 
-if(!module.parent){
-  version = 'IPv4';
-  internal = false;
-  var argv = process.argv;
-  var length = argv.length;
-  if(length >= 3){
-    for(var i = 2; i < length; i++){
-      switch(argv[i]){
-        case '--version6':
-        case '-v6':
-          version = 'IPv6'
-          break;
-        case '--internal':
-        case '-i':
-          internal = true;
-          break;
-      };
-    };
-  };
-  console.log(getMyIP(version,internal));
-}else{
-  module.exports = getMyIP;
-};
+const
+    os         = require( 'os' ),
+    interfaces = os.networkInterfaces();
+
+let version, internal;
+
+function getMyIP( version, internal ) {
+    version  = version || 'IPv4';
+    internal = internal || false;
+
+    for( const key in interfaces ) {
+        const addresses = interfaces[ key ];
+        for( let i = 0; i < addresses.length; i++ ) {
+            const address = addresses[ i ];
+
+            if( address.internal === internal && address.family === version ) {
+                return address.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
+if( !module.parent ) {
+    version    = 'IPv4';
+    internal   = false;
+    let
+        argv   = process.argv,
+        length = argv.length;
+
+    if( length >= 3 ) {
+        for( let i = 2; i < length; i++ ) {
+            if( argv[ i ] === '--version6' || argv[ i ] === '-v6' )
+                version = 'IPv6';
+            else if( argv[ i ] === '--internal' || argv[ i ] === '-i' )
+                internal = true;
+        }
+    }
+
+    return getMyIP( version, internal );
+} else {
+    module.exports = getMyIP;
+}
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-myIP = require('./');
-console.dir(myIP());// return external IPv4
-console.dir(myIP('IPv6'));// return external IPv6
-console.dir(myIP(null,true));// return internal IPv4
-console.dir(myIP('IPv6',true));// return internal IPv6
+'use strict';
+
+const myIP = require( './' );
+
+console.dir( myIP() ); // return external IPv4
+console.dir( myIP( 'IPv6' ) ); // return external IPv6
+console.dir( myIP( null, true ) ); // return internal IPv4
+console.dir( myIP( 'IPv6', true ) ); // return internal IPv6
